16eae0270357cfe39df00e5cc21477110843008b
Deep Raj Khatri <deeprajkhatri117@gmail.com>
2024-08-18 15:04:49 +0530

    check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import logo from "./Hallmark.svg";
 
 const user = process.env.NODE_ENV === 'development'
   ? process.env.REACT_APP_USERNAME // Local development URL
-  :process.env.REACT_APP_USERNAME; // GitHub Pages URLS
+  :process.env.REACT_APP_USERNAME; // GitHub Pages URLSs
 
 const password = process.env.NODE_ENV === 'development'
 ? process.env.REACT_APP_PASSWORD // Local development URL
